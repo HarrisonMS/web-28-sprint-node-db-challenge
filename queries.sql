c347003eee2f1e4113465c5001a0a79389f1d505
Harrison <hmseaborn@gmail.com>
2020-04-17 09:40:57 -0700

    query to dbl check

--- queries.sql
+++ queries.sql
@@ -6,6 +6,9 @@ FROM projects
 SELECT * 
 FROM tasks
 
+SELECT * 
+FROM resources
+
 SELECT p.name, t.id, t.description, t.notes, t.completed,  p.completed
 FROM tasks AS t
 JOIN projects as p on t.project_id = p.id
