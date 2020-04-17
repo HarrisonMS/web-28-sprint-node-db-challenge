861d96e359361c34d88b7b62b4658c3c2c8d7760
Harrison <hmseaborn@gmail.com>
2020-04-17 10:06:55 -0700

    more queries

--- queries.sql
+++ queries.sql
@@ -9,6 +9,16 @@ FROM tasks
 SELECT * 
 FROM resources
 
-SELECT p.name, t.id, t.description, t.notes, t.completed,  p.completed
+SELECT p.name, r.name, r.description, t.id, t.description, t.notes, t.completed,  p.completed
 FROM tasks AS t
 JOIN projects as p on t.project_id = p.id
+JOIN resources AS r on project_resources.resource_id = p.id
+
+SELECT 
+    p.name AS 'Project Name',
+    r.name AS 'Resource Name', 
+    r.description AS 'Resource Description'
+FROM projects AS p
+JOIN resources AS r ON pr.resource_id = r.id
+JOIN project_resources AS pr ON pr.project_id = p.id
+WHERE p.id = 1
