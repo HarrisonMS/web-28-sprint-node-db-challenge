4c214d4da623b47f880a7c7500495473ad840572
Harrison <hmseaborn@gmail.com>
2020-04-17 20:15:07 -0700

    adding project functionalish

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -2,22 +2,32 @@ const db = require('../data/db-config')
 
 module.exports = {
   getProjects,
+  findById,
   getResources,
+  addProject
 
+}
+function findById() {
+	return db("projects").where({ id }).first()
 }
 
 function getProjects() {
 	return db("projects");
 }
 
-function getResources() {
-	return db("resources");
-}
+
+
 function getResources(id) {
   return db('project_resources as pr')
     .select('r.name as resource_name', 'r.description as resource_description')
     .join('resources as r', 'pr.resource_id', 'r.id')
-    .where({id})
-    
-    
+    .where('pr.project_id', id)
+}
+
+function addProject(project) {
+  return db('projects')
+    .insert(project, 'id')
+    .then((id) => {
+      return findById(id)
+    })
 }
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -20,20 +20,54 @@ router.get("/", (req, res) => {
   });
 });
 
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+ Projects.findById(id)
+    .then(project => {
+      if (project) {
+        res.json(project);
+      } else {
+        res.status(404).json({ message: "Could not find project with given id." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get project" });
+    });
+});
+
 router.get('/:id/resources', (req, res) => {
   const { id } = req.params;
 
   Projects.getResources(id)
   .then(resources => {
     if (resources.length) {
-      res.json({resources: resources});
+    return res.json({resources: resources});
     } 
-      res.status(404).json({ message: 'Could not find resources for given recipe id' })
+    return res.status(404).json({ message: 'Could not find resources for given project id' }) 
   })
   .catch(error => {
     console.log(error)
-    res.status(500).json({ message: 'Failed to get resources for given recipe id' });
+    res.status(500).json({ message: 'Failed to get resources for given project id' });
   });
 });
+
+router.post('/', (req, res) => {
+  const projectData = req.body
+
+  Projects.addProject(projectData)
+  .then(project => {
+    if (!project.name) {
+      res.status(400).json({errrorMessage: 'Name for the project is required.. you may choose not to describe the project if you want'})
+    } else {
+      res.status(201).json({data: project})
+    }
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(500).json({ message: error });
+  });
+})
 
 module.exports = router;
